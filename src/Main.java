fc7d258d2a9e2a957895ef5836a7301f5362f2f0
HappynessfromRussia <200mmofhappiness@gmail.com>
2023-01-31 16:44:37 +0400

    Added Map example

--- src/Main.java
+++ src/Main.java
@@ -134,7 +134,28 @@ public class Main {
 
         System.out.println(catTreeSet);
 
+
         // Map
+        Map<Integer, String> statesMap = new HashMap<>();
+
+        statesMap.clear();
+        statesMap.put(1, "Germany");
+        statesMap.put(2,"Spain");
+        statesMap.put(3,"France");
+        statesMap.put(4,"Itally");
+
+        System.out.println(statesMap.keySet());
+
+        statesMap.remove(2);
+
+        for (Map.Entry<Integer, String>item : statesMap.entrySet()){
+            System.out.printf("|Key : %d  | value : %s\n", item.getKey(), item.getValue());
+
+        }
+
+        String first = statesMap.get(2);
+        System.out.println(first);
+
 
 
     }
